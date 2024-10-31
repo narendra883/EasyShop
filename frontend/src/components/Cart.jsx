2a2681b0d6d8b719e1a68b821e99b0950297be08
narendra883 <gundanarendra16@gmail.com>
2024-10-31 09:59:46 +0530

    updated application properties

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Cart = ({ userId }) => {
     const [cart, setCart] = useState([]);
     const [error, setError] = useState("");
+    const [total,setTotal]=useState(0);
 
     useEffect(() => {
         const fetchCartItems = async () => {
@@ -20,6 +21,11 @@ const Cart = ({ userId }) => {
         fetchCartItems();
     }, [userId]); // Dependency array includes userId to refetch if it changes
 
+    useEffect(() => {
+        const newTotal = cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+        setTotal(newTotal);
+    }, [cart]);
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
             <h1 className="text-3xl font-semibold mb-6 text-indigo-600">Your Cart</h1>
@@ -34,7 +40,7 @@ const Cart = ({ userId }) => {
                         <li key={item.cart_id} className="p-6 bg-white rounded-lg shadow-lg">
                             <p className="text-lg font-medium text-gray-700">Product Name: <span className="font-normal">{item.product.name}</span></p>
                             <p className="text-gray-500">Description: {item.product.description}</p>
-                            <p className="text-gray-500">Price: ${item.product.price}</p>
+                            <p className="text-gray-500">Price: ${setTotal+item.product.price}</p>
                             <p className="text-gray-500">Quantity: {item.quantity}</p>
                         </li>
                     ))}
@@ -42,6 +48,9 @@ const Cart = ({ userId }) => {
             ) : (
                 <p className="text-gray-500 mt-4">Your cart is empty.</p>
             )}
+            <div>
+                <h1>Total price:${total.toFixed(2)}</h1>
+            </div>
         </div>
     );
 };
